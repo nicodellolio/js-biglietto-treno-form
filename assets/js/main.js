75464dc74b649abe9dd606da6bd141e1a28b0f5d
nicodellolio <nico.dellolio18@gmail.com>
2024-02-20 17:51:44 +0100

    fix variables names

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,25 +10,34 @@
 // ____________________________________ //
 
 // Get the user name
-const userName = document.getElementById('fullName');
-console.log(userName.value);
+const userNameField = document.getElementById('fullName');
+// console.log(userName.value);
 
 // Get the km the user wants to run
-const km = document.getElementById('kmToRun');
-console.log(km.value);
+const kmField = document.getElementById('kmToRun');
+// console.log(km.value);
 
 // Get the km the user wants to run
-const userAge = document.getElementById('userAge');
-console.log(userAge.value);
+const userAgeField = document.getElementById('userAge');
+// console.log(userAge.value);
+
+// ____________________________________ //
+
+// Add a button that triggers the value from the user's input
 
 
-// Calculate the trip total price:
+document.getElementById('submit').addEventListener('click', function(){
+    console.log(userNameField.value);
+    console.log(userAgeField.value);
+    console.log(kmField.value);
+
+    // Calculate the trip total price:
  // = 0.21€/km
 
- const pricePerKm = (0.21);
+ const pricePerKm = 0.21;
  console.log(pricePerKm);
 
- const tickeFullPrice = (pricePerKm * km);
+ const tickeFullPrice = (pricePerKm * kmField.value);
  console.log(tickeFullPrice);
 
 
@@ -36,25 +45,17 @@ console.log(userAge.value);
     // subtract the 20% discount for the passengers underage (under 18)
 if (userAge < 18) {
     const ticketDiscount20 = ((tickeFullPrice / 100) * 80);
-    console.log(ticketDiscount20);
+    console.log(ticketDiscount20 + "€").toFixed(2);
 
     // sottsubtract the 40% discount for the elder passengers (over 65)
 } else if (userAge >= 65) {
     const ticketDiscount40 = ((tickeFullPrice / 100) * 60);
-    console.log(ticketDiscount40);
-
+    console.log(ticketDiscount40 + "€").toFixed(2);
 
     // no discount for adults passengers (18-64)
 } else {
-    console.log(tickeFullPrice);
+    console.log(tickeFullPrice + "€").toFixed(2);
 }
-
-// ____________________________________ //
-
-// Add a button that triggers the value from the user's input
-
-document.getElementById('submit').addEventListener('click', function(){
-    console.log(userName.value);
-    console.log(userAge.value);
-    console.log(km.value);
 })
+
+
