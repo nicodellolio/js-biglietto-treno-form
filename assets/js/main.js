ec2295ef5df8447664b15ce7166a65c20360e287
nicodellolio <nico.dellolio18@gmail.com>
2024-02-20 16:40:33 +0100

    add  button to display in console the user-input

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,41 +10,50 @@
 // ____________________________________ //
 
 // Get the user name
-const userName = document.getElementById('fullName').value;
-console.log(userName);
+const userName = document.getElementById('fullName');
+console.log(userName.value);
 
 // Get the km the user wants to run
-const km = document.getElementById('kmToRun').value;
-console.log(km);
+const km = document.getElementById('kmToRun');
+console.log(km.value);
 
 // Get the km the user wants to run
-const userAge = document.getElementById('userAge').value;
+const userAge = document.getElementById('userAge');
+console.log(userAge.value);
 
-// calcolare il prezzo totale del viaggio:
+
+// Calculate the trip total price:
  // = 0.21€/km
 
- let pricePerKm = (0.21);
+ const pricePerKm = (0.21);
  console.log(pricePerKm);
 
- let tickeFullPrice = (pricePerKm * km);
+ const tickeFullPrice = (pricePerKm * km);
  console.log(tickeFullPrice);
 
 
-// sottrarre sconto per categoria
-    // sottrare uno sconto del 20% per i minorenni (under 18)
+// subtract the discounts
+    // subtract the 20% discount for the passengers underage (under 18)
 if (userAge < 18) {
     console.log((tickeFullPrice / 100) * 80);
     let ticketDiscount20 = ((tickeFullPrice / 100) * 80);
 
-    // sottrare uno sconto del 40% per gli anziani (over 65)
+    // sottsubtract the 40% discount for the elder passengers (over 65)
 } else if (userAge >= 65) {
     console.log((tickeFullPrice / 100) * 60);
     let ticketDiscount40 = ((tickeFullPrice / 100) * 60);
 
-    // nessuno sconto per i passegeri adulti (18-64)
+    // no discount for adults passengers (18-64)
 } else {
     console.log(tickeFullPrice);
 }
 
 // ____________________________________ //
 
+// Add a button that triggers the value from the user's input
+
+document.getElementById('submit').addEventListener('click', function(){
+    console.log(userName.value);
+    console.log(userAge.value);
+    console.log(km.value);
+})
