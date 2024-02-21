c257db0e1de66a8600733043cf0def305380500a
nicodellolio <nico.dellolio18@gmail.com>
2024-02-21 18:26:15 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,8 @@ const userAgeField = document.getElementById('userAge');
 document.getElementById('submit').addEventListener('click', function(){
     const kmNumber = Number(kmField.value);
     const ageNumber = Number(userAgeField.value);
+    let cabNumber = Math.floor((Math.random() * 12) + 1);
+    let cpCode = Math.floor((Math.random() * 99999) + 33333);
 
     console.log(userNameField.value);
     console.log(ageNumber);
@@ -46,23 +48,40 @@ document.getElementById('submit').addEventListener('click', function(){
  console.log(tickeFullPrice);
 
 
-
-
 // subtract the discounts
     // subtract the 20% discount for the passengers underage (under 18)
 if (ageNumber < 18) {
+
     const ticketDiscount20 = ((tickeFullPrice / 100) * 80);
     console.log(ticketDiscount20.toFixed(2) + "€");
+    document.getElementById('passenger_name').innerHTML = userNameField.value;
+    document.getElementById('offer').innerHTML = `Offerta Junior`;
+    document.getElementById('cab').innerHTML = cabNumber;
+    document.getElementById('CP').innerHTML = cpCode;
+    document.getElementById('price').innerHTML = `€` + ticketDiscount20.toFixed(2)
+
 
     // sottsubtract the 40% discount for the elder passengers (over 65)
 } else if (ageNumber >= 65) {
+
     const ticketDiscount40 = ((tickeFullPrice / 100) * 60);
     console.log(ticketDiscount40.toFixed(2) + "€");
+    document.getElementById('passenger_name').innerHTML = userNameField.value;
+    document.getElementById('offer').innerHTML = `Offerta Senior`;
+    document.getElementById('cab').innerHTML = cabNumber;
+    document.getElementById('CP').innerHTML = cpCode;
+    document.getElementById('price').innerHTML = `€` + ticketDiscount40.toFixed(2)
 
     // no discount for adults passengers (18-64)
 } else {
     console.log(tickeFullPrice.toFixed(2) + "€");
+    document.getElementById('passenger_name').innerHTML = userNameField.value;
+    document.getElementById('offer').innerHTML = `Biglietto Standard`;
+    document.getElementById('cab').innerHTML = cabNumber;
+    document.getElementById('CP').innerHTML = cpCode;
+    document.getElementById('price').innerHTML = `€` + tickeFullPrice.toFixed(2)
 }
 })
+
 
 
